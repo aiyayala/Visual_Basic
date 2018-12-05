b5ed8e455fbe5416debfdfe14325a3d2c8e85af7
aiyayala <45225968+aiyayala@users.noreply.github.com>
2018-12-05 17:34:59 -0500

    Banking

---
+++ Banking/Banking/frmbanking.Designer.vb
@@ -0,0 +1,335 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.lblmon = New System.Windows.Forms.TextBox()
+        Me.lbltue = New System.Windows.Forms.TextBox()
+        Me.lblwed = New System.Windows.Forms.TextBox()
+        Me.lbltr = New System.Windows.Forms.TextBox()
+        Me.lblfri = New System.Windows.Forms.TextBox()
+        Me.lblbegin = New System.Windows.Forms.TextBox()
+        Me.lblending = New System.Windows.Forms.TextBox()
+        Me.lstBX = New System.Windows.Forms.ListBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuSTOpenFile = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuSTReport = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuSTExt = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(81, 19)
+        Me.Label1.TabIndex = 19
+        Me.Label1.Text = "Customers"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(308, 41)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(94, 19)
+        Me.Label2.TabIndex = 20
+        Me.Label2.Text = "Transactions"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(224, 231)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(72, 19)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "Thursday"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(224, 182)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(86, 19)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "Wednesday"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(223, 136)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(65, 19)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Tuesday"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(223, 90)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(65, 19)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Monday"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(223, 278)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(52, 19)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "Friday"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.ForeColor = System.Drawing.Color.Blue
+        Me.Label8.Location = New System.Drawing.Point(224, 324)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(76, 19)
+        Me.Label8.TabIndex = 15
+        Me.Label8.Text = "Beginning"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.ForeColor = System.Drawing.Color.Blue
+        Me.Label9.Location = New System.Drawing.Point(225, 400)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(63, 19)
+        Me.Label9.TabIndex = 18
+        Me.Label9.Text = "Balance"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.ForeColor = System.Drawing.Color.Blue
+        Me.Label10.Location = New System.Drawing.Point(225, 381)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(55, 19)
+        Me.Label10.TabIndex = 17
+        Me.Label10.Text = "Ending"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.ForeColor = System.Drawing.Color.Blue
+        Me.Label11.Location = New System.Drawing.Point(225, 343)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(63, 19)
+        Me.Label11.TabIndex = 16
+        Me.Label11.Text = "Balance"
+        '
+        'lblmon
+        '
+        Me.lblmon.Font = New System.Drawing.Font("Times New Roman", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblmon.Location = New System.Drawing.Point(316, 90)
+        Me.lblmon.Name = "lblmon"
+        Me.lblmon.Size = New System.Drawing.Size(66, 22)
+        Me.lblmon.TabIndex = 1
+        Me.lblmon.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'lbltue
+        '
+        Me.lbltue.Font = New System.Drawing.Font("Times New Roman", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbltue.Location = New System.Drawing.Point(316, 133)
+        Me.lbltue.Name = "lbltue"
+        Me.lbltue.Size = New System.Drawing.Size(66, 22)
+        Me.lbltue.TabIndex = 2
+        Me.lbltue.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'lblwed
+        '
+        Me.lblwed.Font = New System.Drawing.Font("Times New Roman", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblwed.Location = New System.Drawing.Point(316, 182)
+        Me.lblwed.Name = "lblwed"
+        Me.lblwed.Size = New System.Drawing.Size(66, 22)
+        Me.lblwed.TabIndex = 3
+        Me.lblwed.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'lbltr
+        '
+        Me.lbltr.Font = New System.Drawing.Font("Times New Roman", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbltr.Location = New System.Drawing.Point(316, 231)
+        Me.lbltr.Name = "lbltr"
+        Me.lbltr.Size = New System.Drawing.Size(66, 22)
+        Me.lbltr.TabIndex = 4
+        Me.lbltr.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'lblfri
+        '
+        Me.lblfri.Font = New System.Drawing.Font("Times New Roman", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblfri.Location = New System.Drawing.Point(316, 278)
+        Me.lblfri.Name = "lblfri"
+        Me.lblfri.Size = New System.Drawing.Size(66, 22)
+        Me.lblfri.TabIndex = 5
+        Me.lblfri.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'lblbegin
+        '
+        Me.lblbegin.Font = New System.Drawing.Font("Times New Roman", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblbegin.Location = New System.Drawing.Point(312, 343)
+        Me.lblbegin.Name = "lblbegin"
+        Me.lblbegin.Size = New System.Drawing.Size(86, 22)
+        Me.lblbegin.TabIndex = 6
+        Me.lblbegin.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'lblending
+        '
+        Me.lblending.Font = New System.Drawing.Font("Times New Roman", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblending.Location = New System.Drawing.Point(312, 396)
+        Me.lblending.Name = "lblending"
+        Me.lblending.Size = New System.Drawing.Size(86, 22)
+        Me.lblending.TabIndex = 7
+        Me.lblending.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'lstBX
+        '
+        Me.lstBX.FormattingEnabled = True
+        Me.lstBX.Location = New System.Drawing.Point(16, 75)
+        Me.lstBX.Name = "lstBX"
+        Me.lstBX.Size = New System.Drawing.Size(171, 368)
+        Me.lstBX.TabIndex = 0
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(431, 24)
+        Me.MenuStrip1.TabIndex = 21
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuSTOpenFile, Me.MenuSTReport, Me.MenuSTExt})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'MenuSTOpenFile
+        '
+        Me.MenuSTOpenFile.Name = "MenuSTOpenFile"
+        Me.MenuSTOpenFile.Size = New System.Drawing.Size(163, 22)
+        Me.MenuSTOpenFile.Text = "Open File"
+        '
+        'MenuSTReport
+        '
+        Me.MenuSTReport.Name = "MenuSTReport"
+        Me.MenuSTReport.Size = New System.Drawing.Size(163, 22)
+        Me.MenuSTReport.Text = "Summary Report"
+        '
+        'MenuSTExt
+        '
+        Me.MenuSTExt.Name = "MenuSTExt"
+        Me.MenuSTExt.Size = New System.Drawing.Size(163, 22)
+        Me.MenuSTExt.Text = "Exit"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(431, 462)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.lstBX)
+        Me.Controls.Add(Me.lblending)
+        Me.Controls.Add(Me.lblbegin)
+        Me.Controls.Add(Me.lblfri)
+        Me.Controls.Add(Me.lbltr)
+        Me.Controls.Add(Me.lblwed)
+        Me.Controls.Add(Me.lbltue)
+        Me.Controls.Add(Me.lblmon)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Chunhong‘s Transaction Report"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents lblmon As TextBox
+    Friend WithEvents lbltue As TextBox
+    Friend WithEvents lblwed As TextBox
+    Friend WithEvents lbltr As TextBox
+    Friend WithEvents lblfri As TextBox
+    Friend WithEvents lblbegin As TextBox
+    Friend WithEvents lblending As TextBox
+    Friend WithEvents lstBX As ListBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents MenuSTOpenFile As ToolStripMenuItem
+    Friend WithEvents MenuSTReport As ToolStripMenuItem
+    Friend WithEvents MenuSTExt As ToolStripMenuItem
+End Class
---
+++ Banking/Banking/frmbanking.vb
@@ -0,0 +1,111 @@
+﻿'CRN1430 Chunhong Lin  Assignment 5
+
+Option Explicit On
+Option Strict On
+Imports System.IO
+
+
+Public Class frmMain
+    'declearation boolean
+    Public ReadOnly Property EndOfStream As Boolean
+
+    'strings 
+    Dim fields(7), record, arrfirst(19), arrlast(19) As String
+    Dim arrmon(19), arrtue(19), arrwed(19), arrtr(19), arrfri(19), arrbegin(19), arrending(19) As Integer
+    Dim num As Integer = 0
+
+    'openfile
+    Private Sub MenuSTOpenFile_Click(sender As Object, e As EventArgs) Handles MenuSTOpenFile.Click
+        Dim inFile As StreamReader
+        Dim openFileDialog1 As New OpenFileDialog()
+
+        openFileDialog1.InitialDirectory = "c:\"
+        openFileDialog1.Filter = "csv files (*.csv)|*.csv"
+
+        If (openFileDialog1.ShowDialog() = DialogResult.Cancel) Then
+            Exit Sub
+        Else
+            inFile = File.OpenText(openFileDialog1.FileName)
+            Do Until inFile.EndOfStream
+                'seperate from ,
+                record = inFile.ReadLine
+                fields = Split(record, ",")
+
+                'list box name 
+                lstBX.Items.Add(CStr(fields(1)) + "," + CStr(fields(0)))
+
+                'connect the name with the choice
+                arrlast(num) = CStr(fields(0))
+                arrfirst(num) = CStr(fields(1))
+                arrbegin(num) = CInt(fields(2))
+                arrmon(num) = CInt(fields(3))
+                arrtue(num) = CInt(fields(4))
+                arrwed(num) = CInt(fields(5))
+                arrtr(num) = CInt(fields(6))
+                arrfri(num) = CInt(fields(7))
+                arrending(num) = arrbegin(num) + arrmon(num) + arrtue(num) + arrwed(num) + arrtr(num) + arrfri(num)
+                'outfile report
+                num = num + 1
+            Loop
+            'Select the first one
+            lstBX.SelectedIndex = 0
+            inFile.Close()
+        End If
+    End Sub
+
+
+    'outfile
+    Private Sub MenuSTReport_Click(sender As Object, e As EventArgs) Handles MenuSTReport.Click
+        Dim outFile As StreamWriter
+        Dim saveFileDialog1 As New SaveFileDialog()
+        Dim i As Integer
+        saveFileDialog1.Filter = "txt files (*.txt)|*.txt"
+        saveFileDialog1.InitialDirectory = "c:\"
+        saveFileDialog1.FileName = "report.txt"
+
+        If (saveFileDialog1.ShowDialog() = DialogResult.OK) Then
+            outFile = File.CreateText(saveFileDialog1.FileName)
+            'timer for output reprot 
+            outFile.WriteLine("Summary Report Created: " & Date.Now.ToString("dddd, MMMM d, yyyy"))
+            'banner 
+            outFile.WriteLine("  ")
+            outFile.WriteLine(LSet(" ", 20) & LSet("Name", 20) & RSet("Beginning", 20) & RSet("Ending", 40))
+            outFile.WriteLine(LSet(" ", 40) & RSet("Balance", 18) & RSet("Balance", 43))
+            outFile.WriteLine("  ")
+            For i = 0 To num - 1
+                outFile.WriteLine(LSet(" ", 20) & LSet(arrlast(i) & " " & arrfirst(i), 20) & RSet(CType(FormatCurrency(arrbegin(i),,, TriState.False), String), 20) & RSet(CType(FormatCurrency(arrending(i),,, TriState.False), String), 40))
+            Next i
+            MsgBox("Summary Report has been created")
+            outFile.Close()
+        Else
+        MsgBox("No Summary Reprot has been created")
+        End If
+
+    End Sub
+
+
+    Private Sub PopulateLabels(i As Integer)
+        lblmon.Text = FormatNumber(arrmon(i), 0)
+        lbltue.Text = FormatNumber(arrtue(i), 0)
+        lblwed.Text = FormatNumber(arrwed(i), 0)
+        lbltr.Text = FormatNumber(arrtr(i), 0)
+        lblfri.Text = FormatNumber(arrfri(i), 0)
+        lblbegin.Text = FormatNumber(arrbegin(i), 0)
+        lblending.Text = FormatNumber(arrending(i), 0)
+
+    End Sub
+
+    Private Sub lstBX_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstBX.SelectedIndexChanged
+        PopulateLabels(lstBX.SelectedIndex)
+    End Sub
+
+    'Menustrip Exit
+    Private Sub MenuSTExt_Click(sender As Object, e As EventArgs) Handles MenuSTExt.Click
+
+        Dim result = MsgBox("Are you sure you want to exit",
+               MsgBoxStyle.YesNo Or MsgBoxStyle.Exclamation, "Leaving?")
+        If result = MsgBoxResult.Yes Then
+            Application.Exit()
+        End If
+    End Sub
+End Class
